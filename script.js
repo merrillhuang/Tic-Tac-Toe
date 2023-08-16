de10e24afb727d07fea9f429fd25ab1c0b117805
unknown <merrill.huang@gmail.com>
2023-08-16 12:24:24 -0500

    add win and ties counter display

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 console.log("Here!")
 
 let currentSymbol = "X"
+let xWins = 0
+let oWins = 0
+let ties = 0
+
+const xWinsDisplay = document.querySelector("#xWins")
+const oWinsDisplay = document.querySelector("#oWins")
+const tiesDisplay = document.querySelector("#ties")
 
 const newGameButton = document.querySelector("#newgame").addEventListener("click", newGame)
 
@@ -51,16 +58,23 @@ function disableSquare(square) {
 
 function checkGameStatus(symbol) {
     if ((squares[0].innerText === symbol && squares[1].innerText === symbol && squares[2].innerText === symbol) ||
-        (squares[3].innerText === symbol && squares[4].innerText === symbol && squares[5].innerText === symbol) ||
-        (squares[6].innerText === symbol && squares[7].innerText === symbol && squares[8].innerText === symbol) || //rows
-        (squares[0].innerText === symbol && squares[3].innerText === symbol && squares[6].innerText === symbol) ||
-        (squares[1].innerText === symbol && squares[4].innerText === symbol && squares[7].innerText === symbol) ||
-        (squares[2].innerText === symbol && squares[5].innerText === symbol && squares[8].innerText === symbol) || //columns
-        (squares[0].innerText === symbol && squares[4].innerText === symbol && squares[8].innerText === symbol) || 
-        (squares[2].innerText === symbol && squares[4].innerText === symbol && squares[6].innerText === symbol)    //diangonals
+    (squares[3].innerText === symbol && squares[4].innerText === symbol && squares[5].innerText === symbol) ||
+    (squares[6].innerText === symbol && squares[7].innerText === symbol && squares[8].innerText === symbol) || //rows
+    (squares[0].innerText === symbol && squares[3].innerText === symbol && squares[6].innerText === symbol) ||
+    (squares[1].innerText === symbol && squares[4].innerText === symbol && squares[7].innerText === symbol) ||
+    (squares[2].innerText === symbol && squares[5].innerText === symbol && squares[8].innerText === symbol) || //columns
+    (squares[0].innerText === symbol && squares[4].innerText === symbol && squares[8].innerText === symbol) || 
+    (squares[2].innerText === symbol && squares[4].innerText === symbol && squares[6].innerText === symbol)    //diangonals
     ){
         alert(`${symbol} won!`)
+        if (symbol === "X") {
+            xWins++
+        }
+        if (symbol === "O") {
+            oWins++
+        }
         endGame()
+        return
     }
     let allFilled = true
     squares.forEach((square) => {
@@ -69,11 +83,16 @@ function checkGameStatus(symbol) {
         }
     })
     if (allFilled) {
+        ties++
         alert("It's a tie!")
+        endGame()
     }
 }
 
 function endGame() {
     squares.forEach(square => disableSquare(square))
     turnDisplay.innerText = "Game Over!"
+    xWinsDisplay.innerText = `X: ${xWins} wins`
+    oWinsDisplay.innerText = `O: ${oWins} wins`
+    tiesDisplay.innerText = `Ties: ${ties}`
 }
