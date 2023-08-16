235f388362d6d126fdcf6ad3530e1f9fec70f502
unknown <merrill.huang@gmail.com>
2023-08-16 11:53:19 -0500

    add game winning logic and endgame function to stop playing game

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ squares.forEach((square) => {
     square.classList.add("clickable")
     square.addEventListener("click", placeSymbol)
 })
-    
+
+const board = document.querySelector("#boardContainer").addEventListener("click", markClicked)
 
 const turnDisplay = document.querySelector("#turnDisplay")
-
-const board = document.querySelector("#boardContainer").addEventListener("click", clicked)
 
 function placeSymbol(e) {
     e.target.innerText = currentSymbol
     changePlayer(currentSymbol)
+    checkGameStatus(e.target.innerText)
 }
 
 function changePlayer(symbol) {
@@ -40,7 +40,31 @@ function newGame() {
     turnDisplay.innerText = ""
 }
 
-function clicked(e) {
-    e.target.removeEventListener("click", placeSymbol)
-    e.target.classList.remove("clickable")
+function markClicked(e) {
+    disableSquare(e.target)
+}
+
+function disableSquare(square) {
+    square.classList.remove("clickable")
+    square.removeEventListener("click", placeSymbol)
+}
+
+function checkGameStatus(symbol) {
+    if ((squares[0].innerText === symbol && squares[1].innerText === symbol && squares[2].innerText === symbol) ||
+        (squares[3].innerText === symbol && squares[4].innerText === symbol && squares[5].innerText === symbol) ||
+        (squares[6].innerText === symbol && squares[7].innerText === symbol && squares[8].innerText === symbol) || //rows
+        (squares[0].innerText === symbol && squares[3].innerText === symbol && squares[6].innerText === symbol) ||
+        (squares[1].innerText === symbol && squares[4].innerText === symbol && squares[7].innerText === symbol) ||
+        (squares[2].innerText === symbol && squares[5].innerText === symbol && squares[8].innerText === symbol) || //columns
+        (squares[0].innerText === symbol && squares[4].innerText === symbol && squares[8].innerText === symbol) || 
+        (squares[3].innerText === symbol && squares[4].innerText === symbol && squares[6].innerText === symbol)    //diangonals
+    ){
+        alert(`${symbol} won!`)
+        endGame()
+    }
+}
+
+function endGame() {
+    squares.forEach(square => disableSquare(square))
+    turnDisplay.innerText = "Game Over!"
 }
