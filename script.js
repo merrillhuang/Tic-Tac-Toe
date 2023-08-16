ec141a33b26daf4d24f202a42815b802e912d7d0
unknown <merrill.huang@gmail.com>
2023-08-16 10:05:37 -0500

    add functionality to place x on board

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Here!")
+
+const squares = document.querySelectorAll(".square")
+
+let currentSymbol = "x"
+
+squares.forEach((square) => {
+    square.addEventListener("click", () => {
+        square.innerText = currentSymbol
+    })
+})
